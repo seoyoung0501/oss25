463b44ce822a37942b8039ecedee3c394d9ce694
seoyoung0501 <jaguar2005678@naver.com>
2025-06-02 17:21:00 +0900

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,12 @@
+import setuptools
+
+setuptools.setup(
+  name = 'seoyoung',
+  version='0.0.1',
+  description= 'nice calculator',
+  author='seoyoung',
+  url = 'https://github.com/seoyoung0501/oss25',
+  download_url='https://github.com/seoyoung0501/oss25',
+  packages= ['ff'],
+  classifiers=["programming Language ::Python :: 3",
+  ]
